203943811f1522ffa8aa286ebd345e6585c37bb5
Jnakol <weejiriklho@icloud.com>
2025-11-14 11:31:02 +0100

    adssdada

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,4 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Main::index');
+$routes->get('stanice/(:num)', 'Main::stanice/$1');
 
--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -5,11 +5,11 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
 use App\Models\Bundesland;
+use App\Models\Station;
 
 class Main extends BaseController
 {
-    public function index()
-    {
+    public function index(){
         $bundesland = new Bundesland();
         $zeme = $bundesland->findAll();
         //var_dump($zeme);
@@ -17,5 +17,19 @@ class Main extends BaseController
             "zeme" => $zeme
         ];
         echo view('zeme', $data);
+    }
+/**
+ * @param $id - ID země
+ * 
+ * 
+ */
+    public function stanice($id){
+        $bundesland = new Bundesland();
+        $zeme = $bundesland->find($id);
+        $station = new Station();
+        $stanice = $station->where('bundesland', $id)->findAll();
+        var_dump($zeme);
+        var_dump($stanice);
+
     }
 }
